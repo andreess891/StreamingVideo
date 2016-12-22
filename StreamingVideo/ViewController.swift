a4a35a70dd9f4c854370d80d25a2a89d6b6157c3
andreess891 <andreess891@gmail.com>
2016-12-22 15:55:36 -0500

    first

--- StreamingVideo/ViewController.swift
+++ StreamingVideo/ViewController.swift
@@ -7,19 +7,33 @@
 //
 
 import UIKit
+import MediaPlayer
 
 class ViewController: UIViewController {
 
+    var moviePlayer : MPMoviePlayerController?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        playVideo()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    private func playVideo() {
+        let path = "http://10.60.26.243/hls-vod/hls_sample1_manifest.m3u8"
+            let url = NSURL(string:path)
+            moviePlayer = MPMoviePlayerController(contentURL: url as URL!)
+            moviePlayer?.view.frame = self.view.bounds
+            moviePlayer?.prepareToPlay()
+            moviePlayer?.scalingMode = .aspectFill
+            self.view.addSubview((moviePlayer?.view)!)
 
+    }
 
 }
 

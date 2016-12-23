251f700ddde14752b54736e3ef40c3487178df87
andreess891 <andreess891@gmail.com>
2016-12-23 14:39:21 -0500

    Se hacen mejoras en la reproducción del video

--- StreamingVideo/ViewController.swift
+++ StreamingVideo/ViewController.swift
@@ -8,15 +8,18 @@
 
 import UIKit
 import MediaPlayer
+import AVKit
 
 class ViewController: UIViewController {
 
-    var moviePlayer : MPMoviePlayerController?
+    var movPlayer : AVPlayerViewController?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        playVideo()
+
+        //usingAVPlayerViewController()
+        
+        //playStreaming()
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,16 +27,35 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    private func playVideo() {
-        let path = "http://10.60.26.243/hls-vod/hls_sample1_manifest.m3u8"
-            let url = NSURL(string:path)
-            moviePlayer = MPMoviePlayerController(contentURL: url as URL!)
-            moviePlayer?.view.frame = self.view.bounds
-            moviePlayer?.prepareToPlay()
-            moviePlayer?.scalingMode = .aspectFill
-            self.view.addSubview((moviePlayer?.view)!)
-
+    func usingAVPlayerViewController() {
+        let videoURL = NSURL(string: "https://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4")
+        let player = AVPlayer(url: videoURL! as URL)
+        let playerViewController = AVPlayerViewController()
+        playerViewController.player = player
+        
+        self.present(playerViewController, animated: true, completion: nil)
+        
+        self.present(playerViewController, animated: true) {
+            playerViewController.player!.play()
+        }
     }
-
+    
+    private func playStreaming() {
+        let videoURL = NSURL(string: "http://10.60.26.94/hls-vod/hls_sample1_manifest.m3u8")
+        let player = AVPlayer(url: videoURL! as URL)
+        let playerLayer = AVPlayerLayer(player: player)
+        playerLayer.frame = self.view.bounds
+        self.view.layer.addSublayer(playerLayer)
+        player.play()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destination = segue.destination as! AVPlayerViewController
+        let url = URL(string: "http://10.60.26.94/hls-vod/hls_sample1_manifest.m3u8")
+        if let movieURL = url {
+            destination.player = AVPlayer(url: movieURL)
+            destination.player?.play()
+        }
+    }
 }
 
